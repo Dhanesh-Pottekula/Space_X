c4b51abfa05710cfd28d643ea658e4a350e8305e
dhanesh <dhaneshpk359@gmail.com>
2024-01-15 11:48:55 +0530

    links

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import { useDispatch, useSelector } from "react-redux";
 import { increment } from "@/redux/slices/launchesSlice";
