1282196afb2cc067eb2df3a54e1076fa91847db2
dhanesh <dhaneshpk359@gmail.com>
2024-01-15 12:44:27 +0530

    f

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,6 @@
-"use client"
 import Link from 'next/link'
-import {useRouter} from "next/navigation";
+ 
 export default function NotFound() {
-  const router = useRouter();
-  router.push("/profile/Profilepage");
   return (
     <div>
       <h2>Not Found</h2>
